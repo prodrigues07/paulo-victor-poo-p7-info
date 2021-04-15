ccdacf0a06686fe6e207ed27f1f7abc94d13058d
Paulo Rodrigues <64041985+Paulo86-dot@users.noreply.github.com>
2021-04-15 15:34:35 -0300

    Updated em 15-04-2021

--- Atividades para Avaliação/Atividade 1 (13 de Abril)/morse.py
+++ Atividades para Avaliação/Atividade 1 (13 de Abril)/morse.py
@@ -1,3 +1,8 @@
+print("========================")
+print("TRADUTOR DE CÓDIGO MORSE")
+print("========================")
+print("\n")
+
 dictionaryOfMorseCode = {'A': '.-',   'B': '-...',    'C': '-.-.',    'D': '-..',
        'E': '.',    'F': '..-.',    'G': '--.',     'H': '....',
        'I': '..',   'J': '.---',    'K': '-.-',     'L': '.-..',
@@ -12,12 +17,30 @@ dictionaryOfMorseCode = {'A': '.-',   'B': '-...',    'C': '-.-.',    'D': '-..'
         }
 
 i = 1
+inMorse = ''
 while i == 1:
-    texto = input ('Digite seu texto: ').upper()
+    texto = input('Digite seu texto para tradução: ').upper()
     for char in texto:
-        morse = dictionaryOfMorseCode[char]
-        if char != '':
-            print("{} = {}".format(char, morse))
+        if char != ' ':
+            inMorse = inMorse + dictionaryOfMorseCode.get(char) + ' '
         else:
-            print(morse)     
+            inMorse = inMorse + ' '     
     i = i+1
+ 
+print(f'A Tradução em Código Morse é: {inMorse}')
+print('\n')
+decision = int(input('Deseja ver a tradução letra por letra?\n\t1 - Sim\n\t2 - Não\n\tDigite uma Opção: '))
+if decision == 1:
+    print('\n')
+    i = 1
+    morse = ''
+    while i == 1:
+        for char in texto:
+            morse = dictionaryOfMorseCode[char]
+            if char != ' ':
+                print("{} = {}".format(char, morse))
+            else:
+                print(morse)     
+        i = i+1
+else:
+    print('\nObrigado por usar nosso tradutor!')
